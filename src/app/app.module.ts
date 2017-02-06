e03808a8c3d72018f7e60ad4fe6a5c0133a98bd9
Swapnil Singh <swappp7singh@gmail.com>
2017-02-06 15:16:40 +0530

    installed bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
+ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { GithubService } from './github.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,11 @@ import { ProfileComponent } from './profile/profile.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    GithubService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubService {
 
-  constructor() { }
+  private username: string;
+  private client_id = 'a62d81ded58444ae2a36';
+  private client_secret = 'cfa1d540d65586bf3de628e650ccdc91dda6127f';
+
+  constructor(private _http: Http) {
+    console.log('Application ready...!');
+    this.username = 'an0nh4x0r';
+  }
+
+  getUser() {
+    return this._http.get('http://api.github.com/users/' + this.username + '?client_id=' + this.client_id
+      + '&client_secret=' + this.client_secret)
+      .map(res => res.json());
+  }
 
 }
