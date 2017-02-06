5418de0179678d5bfe9ebe164e34cacb1a858bcf
Swapnil Singh <swappp7singh@gmail.com>
2017-02-06 16:12:25 +0530

    project completed

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,13 +8,21 @@ import { GithubService } from '../github.service';
 })
 export class ProfileComponent implements OnInit {
 
-  user: any[];
+  user: any;
+  repos: any[];
+  username: string;
 
   constructor(private _githubService: GithubService) {
-    this._githubService.getUser().subscribe(user => this.user = user);
+    this.user = false;
   }
 
   ngOnInit() {
+  }
+
+  searchUser() {
+    this._githubService.updateUser(this.username);
+    this._githubService.getUser().subscribe(user => this.user = user);
+    this._githubService.getRepos().subscribe(repos => this.repos = repos);
   }
 
 }
